d71b1eed1dcafcb0ba23d59be7fe117952eab1e3
LP Boucher <lp_boucher@hotmail.com>
2018-10-04 00:16:07 +0200

    required mongoose, passport, models, keys, mongo connection, routes

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+require('./models/User');
+require('./services/passport');
+const keys = require('./config/dev');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({'hi': 'there'});
-});
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
