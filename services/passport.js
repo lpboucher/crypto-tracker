a95a754f2787813737c88bc2082ff4fc90c035ec
LP Boucher <lp_boucher@hotmail.com>
2018-10-07 23:39:07 +0200

    added serialization, deserialization of user to allow for use of cookies, added logic to check for existing user of save new user

--- services/passport.js
+++ services/passport.js
@@ -5,13 +5,27 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => done(null, user));
+});
+
 passport.use(new FacebookStrategy({
     clientID: keys.facebookClientID,
     clientSecret: keys.facebookClientSecret,
     callbackURL: '/auth/facebook/callback', 
 },
-(accessToken, refreshToken, profile, done) => {
-        console.log("Auth Done!");
-        done(null, profile);
+async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({facebookId: profile.id});
+
+    if (existingUser) {
+        return done(null, existingUser);
+    }
+    
+    const user = await new User({googleId: profile.id}).save();
+    done(null, user);
     }
 ));
