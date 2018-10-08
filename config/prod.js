659f784b9975377b45b1904dedf8b4f8165c0378
LP Boucher <lp_boucher@hotmail.com>
2018-10-09 00:26:26 +0200

    modified gitignore to only ignore dev key file rather than whole config folder

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+    facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+};
