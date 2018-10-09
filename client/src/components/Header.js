26539a46ca7f6d575c8f397a72a6310e2c1c481b
LP Boucher <lp_boucher@hotmail.com>
2018-10-10 00:17:48 +0200

    added initial material styles to header based on auth status

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 class Header extends Component {
     renderContent() {
         switch(this.props.auth) {
+            case null:
+                return;
             case false:
-                return (<li><a href="/auth/google">Login</a></li>);
+                return (<a href="/auth/google">Login</a>);
             default:
-                return (<li><a href="/auth/logout">Logout</a></li>);
+                return (<a href="/auth/logout">Logout</a>);
         }
     }
 
     render() {
         return (
             <div>
-                {this.renderContent()}
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography style={{ flex: 1 }} />
+                            <Button variant="contained" color="secondary" align="right">{this.renderContent()}</Button> 
+                    </Toolbar>  
+                </AppBar>
             </div>
         );
     }
