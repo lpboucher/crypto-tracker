7cc48a0935a553487728d6550fc6bb3e2d675b3a
LP Boucher <lp_boucher@hotmail.com>
2018-10-09 23:22:51 +0200

    removed temp routes to test auth flow and reinstated correct routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,39 +1,25 @@
 const passport = require('passport');
 
 module.exports = (app) => {
-    app.get(
-        '/auth/google',
+    app.get('/auth/google',
         passport.authenticate('google', {
             scope: ['profile', 'email']
         })
     );
 
-    app.get(
-        '/auth/google/callback',
+    app.get('/auth/google/callback',
         passport.authenticate('google'),
         (req, res) => {
-            res.redirect('/success');
+            res.redirect('/');
         }
     );
 
     app.get('/auth/current_user', (req, res) => {
-        console.log(req.user);
         res.send(req.user);
     });
 
-    app.get(
-        '/auth/logout', (req, res) => {
+    app.get('/auth/logout', (req, res) => {
             req.logout();
-            res.redirect('/out');
-        });
-
-    app.get(
-        '/success', (req, res) => {
-            res.send({'done': 'success!'});
-        });
-
-    app.get(
-        '/out', (req, res) => {
-            res.send({'logged': 'out'});
-        });
+            res.redirect('/');
+    });
 }
