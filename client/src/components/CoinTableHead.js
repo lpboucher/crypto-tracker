8e906fb11c46162b37e7421fc96b8e8b35e15d70
LP Boucher <lp_boucher@hotmail.com>
2018-11-07 23:44:22 +0100

    refactored fields out of component logic

--- client/src/components/CoinTableHead.js
+++ client/src/components/CoinTableHead.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import _ from 'lodash';
 
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 
+const FIELDS = [
+  { label: 'Rank', numeric: 'true'},
+  { label: 'Symbol', numeric: 'false'},
+  { label: 'Name', numeric: 'false'},
+  { label: 'Circulating Supply', numeric: 'true'},
+  { label: 'Price (EUR)', numeric: 'true'},
+  { label: 'Price (USD)', numeric: 'true'},
+  { label: '% change (24h)', numeric: 'true'},
+];
+
 const CoinTableHead = () => {
     return (
         <TableHead>
           <TableRow>
-            <TableCell numeric>Rank</TableCell>
-            <TableCell >Symbol</TableCell>
-            <TableCell >Name</TableCell>
-            <TableCell numeric>Circular Supply</TableCell>
-            <TableCell numeric>Price (EUR)</TableCell>
-            <TableCell numeric>Price (USD)</TableCell>
-            <TableCell numeric>% change (24h)</TableCell>
+          { _.map(FIELDS, ({ label, numeric }) => {
+                    return <TableCell numeric={numeric}>{label}</TableCell>
+                })}
           </TableRow>
         </TableHead>
     );
