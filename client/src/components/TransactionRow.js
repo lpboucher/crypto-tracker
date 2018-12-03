7b220b78dfc5d90ffc17f9a986a196ec5beaf728
LP Boucher <lp_boucher@hotmail.com>
2018-12-04 00:40:40 +0100

    created new row to display transaction data

---
+++ client/src/components/TransactionRow.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
+const TransactionRow = ({index, trade, key}) => {
+    return (
+        <TableRow key={key}>
+            <TableCell component="th" scope="row" numeric>{index}</TableCell>
+            <TableCell>{trade.symbol}</TableCell>
+            <TableCell>{trade.coinName}</TableCell>
+            <TableCell>{trade.type}</TableCell>
+            <TableCell numeric>{trade.price}</TableCell>
+            <TableCell numeric>{trade.quantity}</TableCell>
+        </TableRow>
+    );
+};
+
+export default TransactionRow;
