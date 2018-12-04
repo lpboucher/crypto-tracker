c76a3e25b413d5d47d7fd4cc4707c3251a6669e5
LP Boucher <lp_boucher@hotmail.com>
2018-12-04 23:20:29 +0100

    removed unused dependencies

--- client/src/components/AddTrade.js
+++ client/src/components/AddTrade.js
@@ -4,7 +4,6 @@ import { submitTrade } from '../actions';
 
 import { withStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 
