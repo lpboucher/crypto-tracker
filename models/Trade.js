a06b1d81e3ec6b87317fecdf272a1a92eb178fed
LP Boucher <lp_boucher@hotmail.com>
2018-12-14 19:38:22 +0100

    modified trade schema to store prices in different currencies, also added get method to retrieve total cost as price * quantity

--- models/Trade.js
+++ models/Trade.js
@@ -5,10 +5,36 @@ const tradeSchema = new Schema({
     symbol: String,
     coinName: String,
     type: String,
-    price: Number,
+    price: {
+        amount: Number,
+        currency: {
+            type: String,
+            enum: ['EUR', 'USD', 'ETH', 'BTC']
+        }
+    },
+    /* {
+        EUR: {
+            type: Number
+        },
+        USD: {
+            type: Number
+        },
+        ETH: {
+            type: Number
+        },
+        BTC: {
+            type: Number
+        }
+    }, */
     quantity: Number,
-    totalCost: Number,
     _user: { type: Schema.Types.ObjectId, ref: 'User'},
 });
+
+tradeSchema.set('toObject', { virtuals: true });
+tradeSchema.set('toJSON', { virtuals: true });
+
+tradeSchema.virtual('totalCost').get(function () {
+    return this.price * this.quantity;
+  });
 
 mongoose.model('trades', tradeSchema);
