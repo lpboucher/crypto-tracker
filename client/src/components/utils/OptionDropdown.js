5b0f13d4ac0708f7fc6abacf960080c15175ada4
LP Boucher <lp_boucher@hotmail.com>
2018-12-19 22:13:24 +0100

    abstracted away presentation of dropdown textfield to own component

---
+++ client/src/components/utils/OptionDropdown.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from 'react';
+
+import TextField from '@material-ui/core/TextField';
+
+const OptionDropdown = ({id, label, value, coinChange, options}) => {
+    return (
+        <Fragment>
+            <TextField
+                    id={id}
+                    select
+                    label={label}
+                    value={value}
+                    onChange={coinChange}
+                    SelectProps={{
+                        native: true,
+                        MenuProps: {
+                        width: 250,
+                        },
+                    }}
+                    margin="normal"
+                    variant="outlined"
+                    >
+                    {options.map(option => (
+                        <option key={option} value={option}>
+                        {option}
+                        </option>
+                    ))}
+                </TextField>
+            </Fragment>
+    );
+};
+
+export default OptionDropdown;
