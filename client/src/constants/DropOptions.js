e2dc61479a405a123dbe62797eedeb23f061937c
LP Boucher <lp_boucher@hotmail.com>
2018-12-19 22:12:08 +0100

    added additional option arrays in constants

--- client/src/constants/DropOptions.js
+++ client/src/constants/DropOptions.js
@@ -1,3 +1,5 @@
 export const COINS_TO_QUERY_FOR = [10,20,50,100];
   
 export const TRADE_CURRENCY = ['EUR', 'USD', 'ETH', 'BTC'];
+
+export const ORDER_TYPE = ['Buy', 'Sell'];
--- client/src/constants/Fields.js
+++ client/src/constants/Fields.js
@@ -10,9 +10,9 @@ export const COIN_FIELDS = [
   
 export const TRANSACTION_FIELDS = [
     { label: 'Rank', name: 'rank', numeric: true},
-    { label: 'Symbol', name: 'symbol', numeric: false},
+    { label: 'Symbol', name: 'symbol', numeric: false, type: 'dropdown'},
     { label: 'Name', name: 'coinName', numeric: false},
-    { label: 'Buy/Sell', name: 'type', numeric: false},
+    { label: 'Buy/Sell', name: 'type', numeric: false, type: 'dropdown'},
     { label: 'Quantity', name: 'quantity', numeric: true},
     { label: 'Price', name: 'price_amount', numeric: true},
     { label: 'Paid in', name: 'price_currency', numeric: false, type: 'dropdown'}
