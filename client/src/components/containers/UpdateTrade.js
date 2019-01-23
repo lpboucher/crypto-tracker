f17f1fe3873738229f77c5fea6cccbc4e60c2333
LP Boucher <lp_boucher@hotmail.com>
2019-01-23 22:12:47 +0100

    connected update trade and added open drawer action

--- client/src/components/containers/UpdateTrade.js
+++ client/src/components/containers/UpdateTrade.js
@@ -1,16 +1,19 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
+import { openDrawer, closeDrawer } from '../../actions';
 
 import Button from '@material-ui/core/Button';
 import UpdateIcon from '@material-ui/icons/MoreHorizRounded';
 
 class UpdateTrade extends Component {
     render() {
+        const { openDrawer } = this.props;
         return (
             <Fragment>
                 <Button 
                     color="secondary"
                     align="left"
+                    onClick={openDrawer}
                 >
                     <UpdateIcon />
                 </Button>
@@ -19,10 +22,12 @@ class UpdateTrade extends Component {
     }
 }
 
-/*function mapDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch) {
     return {
-        removeTrade: (id) => dispatch(removeTrade(id)),
+        //removeTrade: (id) => dispatch(removeTrade(id)),
+        openDrawer: () => dispatch(openDrawer()),
+        closeDrawer: () => dispatch(closeDrawer()),
     };
-}*/
+}
 
-export default connect(null, null)(UpdateTrade);
+export default connect(null, mapDispatchToProps)(UpdateTrade);
