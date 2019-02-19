534926915eecdb6170e4ecd681b9208b0520a800
LP Boucher <lp_boucher@hotmail.com>
2019-02-19 22:38:15 +0100

    added selectors to coin reducer

--- client/src/ducks/coins.js
+++ client/src/ducks/coins.js
@@ -20,22 +20,23 @@ export const fetchCoins = () => async dispatch => {
     dispatch({type: FETCH_COINS, payload: filteredRes});
 };
 
-
-//Store Schema
-export const coinSymbolSchema = new schema.Entity('symbols', undefined, {idAttribute: 'symbol'});
-export const coinListSymbolSchema = [ coinSymbolSchema ];
-export const coinRankSchema = new schema.Entity('ranks', undefined, {idAttribute: 'rank'});
-export const coinListRankSchema = [ coinRankSchema ];
-
-
 //Reducer
-export default function reducer(state = null, action) {
+const initialState = {
+    marketData: [],
+    bySymbol: {},
+    allSymbols: [],
+    byRank: {},
+    allRanks: [],
+  };
+
+export default function reducer(state = initialState, action) {
     switch(action.type) {
         case FETCH_COINS:
             const normalizedSymbols = normalize(action.payload, coinListSymbolSchema);
             const normalizedRanks = normalize(action.payload, coinListRankSchema);
             return {
                 ...state,
+                marketData: action.payload,
                 bySymbol: normalizedSymbols.entities.symbols,
                 allSymbols: normalizedSymbols.result,
                 byRank: normalizedRanks.entities.ranks,
@@ -45,4 +46,45 @@ export default function reducer(state = null, action) {
         default:
             return state;
     }
+}
+
+//Store Schema
+export const coinSymbolSchema = new schema.Entity('symbols', undefined, {idAttribute: 'symbol'});
+export const coinListSymbolSchema = [ coinSymbolSchema ];
+export const coinRankSchema = new schema.Entity('ranks', undefined, {idAttribute: 'rank'});
+export const coinListRankSchema = [ coinRankSchema ];
+
+//Selectors
+export const getCoins = (state) => {
+    return state.coins;
+  }
+
+export const getCoinsBySymbol = (state) => {
+    const coins = getCoins(state);
+    return coins.marketData.reduce((obj, item) => {
+        obj[item.symbol] = item;
+     return obj;
+   }, {})
+}
+
+export const getAllCoinSymbols = (state) => {
+    const coins = getCoins(state);
+    return coins.marketData.map(coin => {
+        return coin.symbol
+    })
+}
+
+export const getCoinsByRank = (state) => {
+    const coins = getCoins(state);
+    return coins.marketData.reduce((obj, item) => {
+        obj[item.rank] = item;
+     return obj;
+   }, {})
+}
+
+export const getAllCoinRanks = (state) => {
+    const coins = getCoins(state);
+    return coins.marketData.map(coin => {
+        return coin.rank
+    })
 }
