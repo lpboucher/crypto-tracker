b00ba4b4b03e8c1f36a01e28b8680f219d66c50f
LP Boucher <lp_boucher@hotmail.com>
2019-02-28 22:15:07 +0100

    added selector to aggregate trades to positions

--- client/src/ducks/positions.js
+++ client/src/ducks/positions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import _ from 'lodash';
 
-import { getTrades } from './trades';
+import { getTrades, getAllTradeSymbols } from './trades';
 
 //Action Types
 
@@ -15,9 +15,21 @@ import { getTrades } from './trades';
 export const getPositions = (state) => {
     const trades = getTrades(state);
     return _(trades.tradeList)
-            .groupBy('symbol')
-            .map((objs, key) => ({
-                'symbol': key,
-                'total_quantity': _.sumBy(objs, 'quantity') }))
-            .value();
+            .groupBy("symbol")
+            .mapValues(t => ({
+                date: _.minBy(t, 'date').date,
+                symbol: t[0].symbol,
+                name: t[0].coinName,
+                quantityHeld: _.sumBy(t, 'quantity'),
+                totalCost: {
+                    USD: _.sumBy(t, 'total_cost').USD,
+                    BTC: _.sumBy(t, 'total_cost').BTC
+                },
+                paid_in: t[0].paid_in
+              })).value();
+}
+
+//Need to get only unique symbols, not all
+export const getPositionSymbols = (state) => {
+    return getAllTradeSymbols(state);
 }
